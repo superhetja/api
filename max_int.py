831e7c6b89cc474f4b8357eaac1ca7d06e145a68
Hólmfríður Magnea Hákonardóttir <holmfridur@Holmfriurs-MacBook-Pro.local>
2018-09-04 21:34:12 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -7,6 +7,9 @@ while num_int > 0 :
     if num_int > max_int:
         max_int = num_int
     num_int = int(input("Input a number: "))  
+# gerist eitthvað?
 
 
 print("The maximum is", max_int)    # Do not change this line
+
+#breytingar
