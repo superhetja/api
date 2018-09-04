919fb5af27c98187e757cb389af8f94d62b16f3b
Hólmfríður Magnea Hákonardóttir <holmfridur@Holmfriurs-MacBook-Pro.local>
2018-09-04 21:20:13 +0000

    secound commit

---
+++ max_int.py
@@ -0,0 +1,12 @@
+# User inputs numbers repeatedly until a negative value is entered.
+
+
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+while num_int > 0 :
+    if num_int > max_int:
+        max_int = num_int
+    num_int = int(input("Input a number: "))  
+
+
+print("The maximum is", max_int)    # Do not change this line
